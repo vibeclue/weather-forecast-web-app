ac75c831bb272fe038e012347b88abde60d34233
vbcl <most.possible@mail.ru>
2025-03-31 16:55:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Weather Forecast for the Next Days')
+place = st.text_input("Place: ")
+
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help="Select the number of days to forecast")
+
+option = st.selectbox("Select data of view", ("Temperature", "Sky"))
+
+st.subheader(f"{option} for the next {days} days in {place}")
+
+
