41bf05d07149c5832a39b262c39b012d3cb5bcdd
vbcl <most.possible@mail.ru>
2025-04-01 15:37:38 +0300

    create API which take weather data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
 
+from backend import get_data
+
 st.title('Weather Forecast for the Next Days')
 place = st.text_input("Place: ")
 
@@ -12,13 +14,9 @@ option = st.selectbox("Select data of view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 # визуал
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10", "2022-28-10", "2022-29-10"]
-    temperatures = [10, 11, 16, 20, 30]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
-d, t = get_data(days)
+
+d, t = get_data(place, days, option)
 
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature Celsius"},)
 st.plotly_chart(figure)
