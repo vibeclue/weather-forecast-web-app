c6bd1934ae43dbde82dafadf5245e24881ebcfdc
vbcl <most.possible@mail.ru>
2025-04-02 09:16:54 +0300

    fix number temperature. add try/except when the city is doesnt exist

--- main.py
+++ main.py
@@ -12,26 +12,29 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 
 option = st.selectbox("Select data of view", ("Temperature", "Sky"))
 
-if not place:
-    place = 'Brest'
+
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
 # данные
-filtered_data = get_data(place, days)
 
-# визуал
-if option == 'Temperature':
-    temperatures = [dict['main']['temp'] for dict in filtered_data]
-    dates = [dict['dt_txt'] for dict in filtered_data]
-    figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature Celsius"}, )
-    st.plotly_chart(figure)
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-elif option == 'Sky':
-    sky_data = ["images/" + dict['weather'][0]['main'].lower() + ".png" for dict in filtered_data]
-    print(sky_data)
-    st.image(sky_data, width=115)
+        # визуал
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature Celsius"}, )
+            st.plotly_chart(figure)
 
+        elif option == 'Sky':
+            sky_data = ["images/" + dict['weather'][0]['main'].lower() + ".png" for dict in filtered_data]
+            print(sky_data)
+            st.image(sky_data, width=115)
+    except KeyError:
+        st.text("Hey. Your city doesn't have any weather data")
 
 
 
