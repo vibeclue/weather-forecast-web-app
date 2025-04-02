76bc74f315163ab64955ca117ff0b8489c4e1439
vbcl <most.possible@mail.ru>
2025-04-02 09:08:01 +0300

    create temperature plot + sky images

--- backend.py
+++ backend.py
@@ -2,20 +2,16 @@ import requests
 
 API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
     response = requests.get(url)
     content = response.json()
     filtered_data = content['list']
     nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
-    return filtered_data
+    return filtered_data[:nr_values]
+
 
 
 if __name__ == '__main__':
-    print(get_data(place='Tokyo', forecast_days=3, kind='Temperature'))
+    print(get_data(place='Tokyo', forecast_days=3))
 
--- main.py
+++ main.py
@@ -6,20 +6,33 @@ from backend import get_data
 st.title('Weather Forecast for the Next Days')
 place = st.text_input("Place: ")
 
+
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of days to forecast")
 
 option = st.selectbox("Select data of view", ("Temperature", "Sky"))
 
+if not place:
+    place = 'Brest'
+
 st.subheader(f"{option} for the next {days} days in {place}")
 
+# данные
+filtered_data = get_data(place, days)
+
 # визуал
+if option == 'Temperature':
+    temperatures = [dict['main']['temp'] for dict in filtered_data]
+    dates = [dict['dt_txt'] for dict in filtered_data]
+    figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature Celsius"}, )
+    st.plotly_chart(figure)
+
+elif option == 'Sky':
+    sky_data = ["images/" + dict['weather'][0]['main'].lower() + ".png" for dict in filtered_data]
+    print(sky_data)
+    st.image(sky_data, width=115)
 
 
-d, t = get_data(place, days, option)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature Celsius"},)
-st.plotly_chart(figure)
 
 
 
